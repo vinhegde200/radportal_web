e00a9d46220c83cb71b8c436804454b392e148e6
Vinayak Hegde <vinayak.hegde@epssw.com>
2024-05-06 11:00:44 +0530

    Added junit report

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client: {
       jasmine: {
@@ -33,7 +34,12 @@ module.exports = function (config) {
         { type: 'json-summary'}
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: 'reports',
+      outputFile: 'junit.xml',
+      useBrowserName: false
+    },
     browsers: ['Chrome'],
     restartOnFileChange: true
   });
